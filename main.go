bba36461e384050908beeab4e0aabfe3a890e4bf
your_github_username <paerarason1111@gmail.com>
2023-11-12 09:29:50 +0530

    round

--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-package main
+package Loadbalancer
 
 import (
     "encoding/json"
@@ -12,32 +12,31 @@ import (
     "time"
 )
 
-// Config is a configuration.
+
 type Config struct {
     Proxy    Proxy     `json:"proxy"`
     Backends []Backend `json:"backends"`
 }
 
-// Proxy is a reverse proxy, and means load balancer.
+
 type Proxy struct {
     Port string `json:"port"`
 }
 
-// Backend is servers which load balancer is transferred.
 type Backend struct {
     URL    string `json:"url"`
     IsDead bool
     mu     sync.RWMutex
 }
 
-// SetDead updates the value of IsDead in Backend.
+
 func (backend *Backend) SetDead(b bool) {
     backend.mu.Lock()
     backend.IsDead = b
     backend.mu.Unlock()
 }
 
-// GetIsDead returns the value of IsDead in Backend.
+
 func (backend *Backend) GetIsDead() bool {
     backend.mu.RLock()
     isAlive := backend.IsDead
@@ -45,13 +44,15 @@ func (backend *Backend) GetIsDead() bool {
     return isAlive
 }
 
+
 var mu sync.Mutex
 var idx int = 0
+var cfg Config
 
-// lbHandler is a handler for loadbalancing
 func lbHandler(w http.ResponseWriter, r *http.Request) {
     maxLen := len(cfg.Backends)
-    // Round Robin
+    
+	
     mu.Lock()
     currentBackend := cfg.Backends[idx%maxLen]
     if currentBackend.GetIsDead() {
@@ -65,15 +66,15 @@ func lbHandler(w http.ResponseWriter, r *http.Request) {
     mu.Unlock()
     reverseProxy := httputil.NewSingleHostReverseProxy(targetURL)
     reverseProxy.ErrorHandler = func(w http.ResponseWriter, r *http.Request, e error) {
-        // NOTE: It is better to implement retry.
-        log.Printf("%v is dead.", targetURL)
+        
+		log.Printf("%v is dead.", targetURL)
         currentBackend.SetDead(true)
         lbHandler(w, r)
     }
     reverseProxy.ServeHTTP(w, r)
 }
 
-// pingBackend checks if the backend is alive.
+
 func isAlive(url *url.URL) bool {
     conn, err := net.DialTimeout("tcp", url.Host, time.Minute*1)
     if err != nil {
@@ -84,7 +85,7 @@ func isAlive(url *url.URL) bool {
     return true
 }
 
-// healthCheck is a function for healthcheck
+
 func healthCheck() {
     t := time.NewTicker(time.Minute * 1)
     for {
@@ -108,9 +109,9 @@ func healthCheck() {
 
 }
 
-var cfg Config
 
-// Serve serves a loadbalancer.
+
+
 func Serve() {
     data, err := ioutil.ReadFile("./config.json")
     if err != nil {
