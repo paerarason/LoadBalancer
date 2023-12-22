1faebefb411276c8e422e6f1585e3a4899edc8da
your_github_username <paerarason1111@gmail.com>
2023-12-22 19:07:43 +0530

    robin

---
+++ balancers/Robin.go
@@ -0,0 +1,131 @@
+package balancers
+
+import (
+    "encoding/json"
+    "io/ioutil"
+    "log"
+    "net"
+    "net/http"
+    "net/http/httputil"
+    "net/url"
+    "sync"
+    "time"
+)
+
+
+type Config struct {
+    Proxy    Proxy     `json:"proxy"`
+    Backends []Backend `json:"backends"`
+}
+
+
+type Proxy struct {
+    Port string `json:"port"`
+}
+
+type Backend struct {
+    URL    string `json:"url"`
+    IsDead bool
+    mu     sync.RWMutex
+}
+
+
+func (backend *Backend) SetDead(b bool) {
+    backend.mu.Lock()
+    backend.IsDead = b
+    backend.mu.Unlock()
+}
+
+
+func (backend *Backend) GetIsDead() bool {
+    backend.mu.RLock()
+    isAlive := backend.IsDead
+    backend.mu.RUnlock()
+    return isAlive
+}
+
+
+var mu sync.Mutex
+var idx int = 0
+var cfg Config
+
+func lbHandler(w http.ResponseWriter, r *http.Request) {
+    maxLen := len(cfg.Backends)
+    
+	
+    mu.Lock()
+    currentBackend := cfg.Backends[idx%maxLen]
+    if currentBackend.GetIsDead() {
+        idx++
+    }
+    targetURL, err := url.Parse(cfg.Backends[idx%maxLen].URL)
+    if err != nil {
+        log.Fatal(err.Error())
+    }
+    idx++
+    mu.Unlock()
+    reverseProxy := httputil.NewSingleHostReverseProxy(targetURL)
+    reverseProxy.ErrorHandler = func(w http.ResponseWriter, r *http.Request, e error) {
+        
+		log.Printf("%v is dead.", targetURL)
+        currentBackend.SetDead(true)
+        lbHandler(w, r)
+    }
+    reverseProxy.ServeHTTP(w, r)
+}
+
+
+func isAlive(url *url.URL) bool {
+    conn, err := net.DialTimeout("tcp", url.Host, time.Minute*1)
+    if err != nil {
+        log.Printf("Unreachable to %v, error:", url.Host, err.Error())
+        return false
+    }
+    defer conn.Close()
+    return true
+}
+
+
+func healthCheck() {
+    t := time.NewTicker(time.Minute * 1)
+    for {
+        select {
+        case <-t.C:
+            for _, backend := range cfg.Backends {
+                pingURL, err := url.Parse(backend.URL)
+                if err != nil {
+                    log.Fatal(err.Error())
+                }
+                isAlive := isAlive(pingURL)
+                backend.SetDead(!isAlive)
+                msg := "ok"
+                if !isAlive {
+                    msg = "dead"
+                }
+                log.Printf("%v checked %v by healthcheck", backend.URL, msg)
+            }
+        }
+    }
+
+}
+
+
+
+
+func Serve() {
+    data, err := ioutil.ReadFile("./config.json")
+    if err != nil {
+        log.Fatal(err.Error())
+    }
+    json.Unmarshal(data, &cfg)
+
+    go healthCheck()
+
+    s := http.Server{
+        Addr:    ":" + cfg.Proxy.Port,
+        Handler: http.HandlerFunc(lbHandler),
+    }
+    if err = s.ListenAndServe(); err != nil {
+        log.Fatal(err.Error())
+    }
+}
